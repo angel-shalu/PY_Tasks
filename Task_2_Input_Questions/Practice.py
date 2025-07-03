8bcc2ad37bdd5ab00d17a6289b4b5ec7efcf515b
angel-shalu <shalinikumari8789@gamil.com>
2025-07-03 12:45:49 +0530

    Added the code

--- Task_2_Input_Questions/Practice.py
+++ Task_2_Input_Questions/Practice.py
@@ -1,19 +1,19 @@
-#  What is the input() function in Python used for? 
+# 1. What is the input() function in Python used for? 
 """"
- **Answer:** The `input()` function is used to read input from the user as a string. """
+ Answer: The `input()` function is used to read input from the user as a string. """
  
  
  
-# 2. **Question:** How can you accept an integer as input from the user using input()`? 
-"""**Answer:** You can convert the string returned by `input()` into an integer using the 
+# 2. Question: How can you accept an integer as input from the user using input()`? 
+"""Answer: You can convert the string returned by `input()` into an integer using the 
 `int()` function:   """
 
 num = int(input("Enter an integer: ")) 
 
 
 
-# 3. **Question:** How can you accept an integer as input from the user using input()`?
+# 3. Question: How can you accept an integer as input from the user using input()`?
 num = float(input("Enter a float: ")) 
  
  
- 
+
