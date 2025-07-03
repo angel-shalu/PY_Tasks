13ff6d14f3b70144fc948267f056376c01aa77ab
angel-shalu <shalinikumari8789@gamil.com>
2025-07-03 13:44:13 +0530

    modified

--- Task_2_Input_Questions/split.py
+++ Task_2_Input_Questions/split.py
@@ -1,5 +1,5 @@
 
-#  4. Question: How you can take multiple space-separtated value as input() 
+# Question: How you can take multiple space-separtated value as input() 
 """Answer: You can use `split()` to split the input string into a list of values: """
 
 values = input("Enter values: ").split()
@@ -13,5 +13,19 @@ print(numbers)
 # Tking float
 numbers = list(map(float, input("Enter the numbers: ").split()))
 print(numbers)
+
+
+
+
+
+# Question: How do u convert user input to a list of integers 
+"""Answer: After using split(), you can convert each element to an integer using a 
+list comprehension: """
+
+num = [int(x)for x in input("Enter the numbers :").split()]
+
+# Here Printing the list
+num = [int(x)for x in input("Enter the numbers :").split()]
+print(num)
 
 
--- Task_2_Input_Questions/tempCodeRunnerFile.py
+++ Task_2_Input_Questions/tempCodeRunnerFile.py
@@ -1,2 +1,16 @@
 
-# num = int(input("Enter an integer: ")) 
+# # Question: How you can take multiple space-separtated value as input() 
+# """Answer: You can use `split()` to split the input string into a list of values: """
+
+# values = input("Enter values: ").split()
+# print(values)
+
+# # Take space seprate integers
+# numbers = list(map(int, input("Enter numbers: ").split()))
+# print(numbers)
+
+
+# # Tking float
+# numbers = list(map(float, input("Enter the numbers: ").split()))
+# print(numbers)
+
